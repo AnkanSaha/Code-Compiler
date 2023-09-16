17767983f282f31081ced2a0cd445b3025b25208
Ankan Saha <ankansahadaluabari@gmail.com>
2023-09-16 22:38:23 +0530

    Added : Added Typing Animation in Code Entry board

--- client/src/Components/Code/Code Enter Side.jsx
+++ client/src/Components/Code/Code Enter Side.jsx
@@ -1,9 +1,32 @@
 /* eslint-disable no-unused-vars */
 import React from "react"; // Import react module
+import Typed from "typed.js"; // Import Typed.js
 
 // Redux
-import { useDispatch, useSelector } from "react-redux"; // Import useDispatch hook
-import { setLanguage, setCode, setSessionID } from "@redux/Components/Code"; // Import setCode action
+import { useDispatch } from "react-redux"; // Import useDispatch hook
+import {
+  setLanguage,
+  setCode,
+  setSessionID,
+  setFileName,
+} from "@redux/Components/Code"; // Import setCode action
+
+// Import Components
+import {
+  Button,
+  ModalFooter,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  FormControl,
+  FormLabel,
+  Input,
+  useDisclosure,
+  useToast,
+} from "@chakra-ui/react"; // Chakra UI Button
 
 // Import Router
 import { useParams } from "react-router-dom"; // Import useParams hook
@@ -15,10 +38,34 @@ export default function CodeEnterSide() {
   // Hooks
   const { language } = useParams(); // Get language from URL
   const dispatch = useDispatch(); // Get dispatch from useDispatch hook
-  const {Code} = useSelector((state) => state.Code); // Get state from useSelector hook
+  const { isOpen, onOpen, onClose } = useDisclosure(); // Get isOpen, onOpen, onClose from useDisclosure hook
+  const ToastMessage = useToast(); // Get toast from useToast hook
 
+  // States
+  const [InitialCodeSnippet, setInitialCodeSnippet] = React.useState(""); // Set CodeSnippet to empty
+  const [FileName, UpdateFileName] = React.useState(""); // Set FileName to empty
 
+  // References
+  const InitialCodeSnippetRef = React.useRef(null); // Set InitialCodeSnippetRef to null
+
+  // Effects for InitialCodeSnippet animation
   React.useEffect(() => {
+    const typed = new Typed(InitialCodeSnippetRef.current, {
+      strings: [InitialCodeSnippet],
+      typeSpeed: 10,
+      loop: false,
+      cursorChar: "",
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, [InitialCodeSnippet]);
+
+  // Effects for Code finding right code snippet
+  React.useEffect(() => {
+    onOpen(); // Open Modal
     const matchedElement = CodeSnippet.find(
       (element) => element.Language === language
     );
@@ -27,33 +74,79 @@ export default function CodeEnterSide() {
       dispatch(setCode(matchedElement.Code)); // Set code snippet to redux
       dispatch(setLanguage(matchedElement.Language)); // Set language to redux
       dispatch(setSessionID(crypto.randomUUID())); // Set session ID to redux
+      setInitialCodeSnippet(matchedElement.Code); // Set CodeSnippet to matchedElement.Code
     } else {
       dispatch(setCode("")); // Set code snippet to redux to empty
       dispatch(setLanguage("")); // Set language to redux to empty
       dispatch(setSessionID("")); // Set session ID to redux to empty
+      setInitialCodeSnippet(""); // Set CodeSnippet to empty
     }
-  }, [dispatch, language]);
+  }, [dispatch, language, onOpen]);
 
   // Onchange Event Listener
   const CodeEntry = (event) => {
     dispatch(setCode(event.target.value)); // Set code snippet to redux
   };
 
+  // Onchange Event Listener
+  const onFileNameChange = (e) => {
+    UpdateFileName(e.target.value);
+  };
 
+  // On Submit Event Listener
+  const SetFileNameInRedux = () => {
+    dispatch(setFileName(FileName)); // Set FileName in Redux
+    onClose(); // Close Modal
+    ToastMessage({
+      title: "File Name Set Successfully",
+      description: `File Name Set Successfully, Now you can compile your code with ${FileName}`,
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+    });
+    UpdateFileName(""); // Clear FileName State
+  };
   return (
-    <div className="mx-5 mt-10">
-      <textarea
-        onChange={CodeEntry}
-        rows="18"
-        cols="48"
-        className="mockup-code w-6/12 px-10"
-        placeholder={`Enter or Paste your ${language} code here`}
-        defaultValue={Code}
-        wrap="off"
-        autoCapitalize="off"
-        autoCorrect="off"
-        spellCheck="false"
-      ></textarea>
-    </div>
+    <>
+      <div className="mx-5 mt-10">
+        <textarea
+          onChange={CodeEntry}
+          ref={InitialCodeSnippetRef}
+          rows="18"
+          cols="48"
+          className="mockup-code w-6/12 px-10"
+          placeholder={`Enter or Paste your ${language} code here`}
+          wrap="off"
+          autoCapitalize="off"
+          autoCorrect="off"
+          spellCheck="false"
+        ></textarea>
+      </div>
+      {/* Add Packages Modal */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Set File Name</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>File Name with extension</FormLabel>
+              <Input
+                onChange={onFileNameChange}
+                value={FileName}
+                placeholder="Enter The exact file name with extension"
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button onClick={SetFileNameInRedux} colorScheme="green" mr={3}>
+              Set File Name
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
