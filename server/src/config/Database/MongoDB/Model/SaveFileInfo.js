7123b71c787995e9fc9073de17650f34fc4f5d4d
Ankan Saha <ankansahadaluabari@gmail.com>
2023-12-28 19:19:06 +0530

    Deleted : Deleted All Server Side Codes

--- server/src/config/Database/MongoDB/Model/SaveFileInfo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    FileName: {type: String, required: true, unique: true},
-    BuildTime: {type: Date, required: true, default: Date.now},
-    BuildSession: {type: String, required: true, unique: true},
-    ProgrammingLanguage: {type: String, required: true, index: true},
-    FileSize: {type: Number, required: true, index: true},
-}
