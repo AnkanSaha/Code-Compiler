68165e26576d60e4a533a3670ee11a8227811404
Ankan Saha <ankansahadaluabari@gmail.com>
2024-01-05 15:55:40 +0530

    Added : Added Express Server

--- client/src/App/App_Config.js
+++ client/src/App/App_Config.js
@@ -1,6 +1,6 @@
 export const isDevelopment = import.meta.env.DEV; // Check if the app is in development mode
 export const AppName = 'Code Editor'; // Name of the App
-export const API_URL = isDevelopment === true ? 'http://localhost:3000' : window.location.origin; // API URL
+export const API_URL = isDevelopment === true ? 'http://localhost:4896' : window.location.origin; // API URL
 
 // API Controller
 import {React as Service} from 'react-caches'; // Import React Caches
---
+++ server/source/core/environment variables.js
@@ -0,0 +1,31 @@
+/* eslint-disable max-len */
+import dotenv from 'dotenv'; // https://www.npmjs.com/package/dotenv
+dotenv.config(); // Initialize dotenv
+import {cpus, platform, freemem, arch} from 'os'; // Import OS
+
+// Number Keys
+export const NumberKeys = {
+  PORT: Number(process.env.PORT) || 4896, // Port
+  CPUCount: cpus().length * Number(process.env.CPU_COUNT_MULTIPLIERenv) || 2, // CPU Count
+};
+
+// Export keys
+export const StringKeys = {
+  AppName: String('Code-Editor'),
+  CORS_URL: String(process.env.ALLOWED_ORIGINS) || '*',
+  JWT_SECRET: String(process.env.JWT_SECRET),
+  JWT_EXPIRES_IN: String(process.env.JWT_EXPIRES_IN) || '10d',
+  StaticDirectoryName: String('Code'),
+  // Server Details
+  Platform: String(platform()),
+  Architecture: String(arch()),
+  FreeRam: Number((freemem() / 1024 / 1024 / 1024).toFixed(2)),
+  Model: String(cpus()[0].model),
+};
+
+// Database Keys
+export const DatabaseKeys = {
+  MongoDB: String(process.env.MONGODB_URI),
+  DB_Name: String(process.env.DB_NAME) || 'code-editor',
+  CompiledFileRecord: String('compiledFileRecord'),
+};
