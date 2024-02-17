117a8be68d258f20e19e7e848d00f45b21d52a81
Ankan Saha <ankansahadaluabari@gmail.com>
2024-02-17 22:23:27 +0000

    Fixed : Fixed Outers Package Version

--- server/source/Controllers/CompileCode.controller.js
+++ server/source/Controllers/CompileCode.controller.js
@@ -1,37 +1,37 @@
-import fs from 'fs' // File System Module
-import { join } from 'path' // Import the path module
-import PackageInstaller from './Functions/Package-Installer.function.controllers.js' // Import Package Installer
-import { Serve, StatusCodes } from 'outers' // Import Response from Outers
-import { LangTypesDirectory } from '../core/environment variables.core.js' // Environmental Variables
-import Executor from './Functions/executor.function.controllers.js' // Import Executor
+import fs from 'fs'; // File System Module
+import {join} from 'path'; // Import the path module
+import PackageInstaller from './Functions/Package-Installer.function.controllers.js'; // Import Package Installer
+import {Serve, StatusCodes} from 'outers'; // Import Response from Outers
+import {LangTypesDirectory} from '../core/environment variables.core.js'; // Environmental Variables
+import Executor from './Functions/executor.function.controllers.js'; // Import Executor
 
 // Import MongoDB
-import { MongooseModel } from '../Database/MongoDB.db.js' // Import MongoDB
+import {MongooseModel} from '../Database/MongoDB.db.js'; // Import MongoDB
 
 // Main Compile Code Controller
-export default async function Compile (Request, Response) {
+export default async function Compile(Request, Response) {
   // Write Code to Uncompiled File Directory
-  const { SessionID, Language, Code, FileName, Packages, RequesterIPaddress } = Request.body // Destructure Request Body
+  const {SessionID, Language, Code, FileName, Packages, RequesterIPaddress} = Request.body; // Destructure Request Body
 
   // Select Preferred Directory
-  const PreferredLanguageDir = LangTypesDirectory.find((element) => element.language.toLowerCase() === Language.toLowerCase()) // Find Preferred Directory
+  const PreferredLanguageDir = LangTypesDirectory.find((element) => element.language.toLowerCase() === Language.toLowerCase()); // Find Preferred Directory
 
   // Check if Preferred Directory Exists in Environmental Variables
-  const FilePath = `${join(`${PreferredLanguageDir.directoryName}/${SessionID}-${FileName}`)}` // Path of Uncompiled File
-  const PreferredFileName = `${SessionID}-${FileName}` // File Name in Server
+  const FilePath = `${join(`${PreferredLanguageDir.directoryName}/${SessionID}-${FileName}`)}`; // Path of Uncompiled File
+  const PreferredFileName = `${SessionID}-${FileName}`; // File Name in Server
 
   // Find sessionID in MongoDB if it exists
-  const ExistSessionID = await MongooseModel.find({ sessionID: SessionID }) // Find SessionID in MongoDB
+  const ExistSessionID = await MongooseModel.find({sessionID: SessionID}); // Find SessionID in MongoDB
 
   // Check if SessionID exists in MongoDB
   if (ExistSessionID.length === 0) {
     // // Write Code to File
-    await fs.promises.writeFile(FilePath, Code, 'utf-8') // Write Code to File System if SessionID does not exist in MongoDB
+    await fs.promises.writeFile(FilePath, Code, 'utf-8'); // Write Code to File System if SessionID does not exist in MongoDB
 
     // Check if Any Packages are Required
     if (Packages.length > 0) {
       // Install Packages
-      await PackageInstaller(PreferredLanguageDir, Packages) // Install Packages
+      await PackageInstaller(PreferredLanguageDir, Packages); // Install Packages
     }
 
     // Create a mongoDB document for the SessionID if it does not exist
@@ -44,9 +44,9 @@ export default async function Compile (Request, Response) {
       LanguageName: Language,
       BuilderIP: RequesterIPaddress,
       BuildTime: Date.now(),
-      BuildStatus: 'Pending'
-    }) // Create MongoDB Document
-    const DataStatus = await CompilerDataModel.save() // Save MongoDB Document
+      BuildStatus: 'Pending',
+    }); // Create MongoDB Document
+    const DataStatus = await CompilerDataModel.save(); // Save MongoDB Document
 
     // Check if MongoDB Document was saved
     if (!DataStatus) {
@@ -56,35 +56,35 @@ export default async function Compile (Request, Response) {
         statusCode: StatusCodes.REQUEST_TIMEOUT,
         Title: 'Unable to Compile Code',
         message: 'Unable to Compile Code, please try again later or contact support',
-        data: undefined
-      })
-      return // Return if MongoDB Document was not saved
+        data: undefined,
+      });
+      return; // Return if MongoDB Document was not saved
     }
 
-    await Executor(PreferredFileName, PreferredLanguageDir, SessionID, FilePath, RequesterIPaddress, Response) // Execute Code
+    await Executor(PreferredFileName, PreferredLanguageDir, SessionID, FilePath, RequesterIPaddress, Response); // Execute Code
   } else if (ExistSessionID.length > 0) {
     // Delete Previous File if Same SessionID Exists
-    await fs.promises.unlink(ExistSessionID[0].FilePath) // Delete Previous File if Same SessionID Exists
+    await fs.promises.unlink(ExistSessionID[0].FilePath); // Delete Previous File if Same SessionID Exists
 
     // // Write Code to File
-    await fs.promises.writeFile(ExistSessionID[0].FilePath, Code, 'utf-8') // Write Code to File System if SessionID does not exist in MongoDB
+    await fs.promises.writeFile(ExistSessionID[0].FilePath, Code, 'utf-8'); // Write Code to File System if SessionID does not exist in MongoDB
 
     // Check if Any Packages are Required
     if (Packages.length > 0) {
       // Install Packages
-      await PackageInstaller(PreferredLanguageDir, Packages) // Install Packages
+      await PackageInstaller(PreferredLanguageDir, Packages); // Install Packages
     }
     // Update MongoDB Document for the SessionID if it exists
     const updateStatus = await MongooseModel.updateOne(
-      { sessionID: SessionID },
-      {
-        BuildStatus: 'Pending',
-        BuildTime: Date.now(),
-        BuilderIP: RequesterIPaddress,
-        FileSize: Code.length,
-        FileExtraPackages: Packages
-      }
-    ) // Update MongoDB Document for the SessionID
+        {sessionID: SessionID},
+        {
+          BuildStatus: 'Pending',
+          BuildTime: Date.now(),
+          BuilderIP: RequesterIPaddress,
+          FileSize: Code.length,
+          FileExtraPackages: Packages,
+        },
+    ); // Update MongoDB Document for the SessionID
 
     // Check if MongoDB Document was updated
     if (updateStatus.modifiedCount === 0) {
@@ -94,12 +94,12 @@ export default async function Compile (Request, Response) {
         statusCode: StatusCodes.REQUEST_TIMEOUT,
         Title: 'Unable to Compile Code',
         message: 'Unable to Compile Code, please try again later or contact support',
-        data: undefined
-      })
-      return // Return if MongoDB Document was not updated
+        data: undefined,
+      });
+      return; // Return if MongoDB Document was not updated
     }
 
-    await Executor(PreferredFileName, PreferredLanguageDir, SessionID, FilePath, RequesterIPaddress, Response) // Execute Code
+    await Executor(PreferredFileName, PreferredLanguageDir, SessionID, FilePath, RequesterIPaddress, Response); // Execute Code
   } else {
     Serve.JSON({
       response: Response,
@@ -107,7 +107,7 @@ export default async function Compile (Request, Response) {
       statusCode: StatusCodes.REQUEST_TIMEOUT,
       Title: 'Unable to Compile Code',
       message: 'Unable to Compile Code, please try again later or contact support',
-      data: undefined
-    })
+      data: undefined,
+    });
   }
 }
--- server/source/Routers/Router.js
+++ server/source/Routers/Router.js
@@ -1,30 +1,30 @@
-import { Router } from 'express' // Router from express
-import { StatusCodes, Serve } from 'outers' // Import Status Codes
-import CORS from 'cors' // Cors Module
-import { StringKeys } from '../core/environment variables.core.js' // CORS Config
+import {Router} from 'express'; // Router from express
+import {StatusCodes, Serve} from 'outers'; // Import Status Codes
+import CORS from 'cors'; // Cors Module
+import {StringKeys} from '../core/environment variables.core.js'; // CORS Config
 
 // import All Sub Routers
-import CompileRouter from './Routes/CompileCode.Routes.js' // Environment Variables
+import CompileRouter from './Routes/CompileCode.Routes.js'; // Environment Variables
 
 // setup Router
-const MainRouter = Router() // Main Router
+const MainRouter = Router(); // Main Router
 
 // Setup Cors Config
 MainRouter.use(
-  CORS({
-    origin: StringKeys.CORS_URL, // Allow Only This URL to Access
-    credentials: true, // Allow Client To Send Cookies or Credentials to Server
-    allowedHeaders: ['Authorization', 'Content-Type', 'filename'], // Allow Only These Headers from Client to Server
-    exposedHeaders: ['filename', 'Content-Disposition'], // Expose Custom Headers to Client from Server
-    maxAge: 86400, // 1 Day
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow Only These Methods
-    optionsSuccessStatus: StatusCodes.CONTINUE, // Set Status Code for OPTIONS Request
-    preflightContinue: true // Continue if OPTIONS Request
-  })
-) // Compile Code Router
+    CORS({
+      origin: StringKeys.CORS_URL, // Allow Only This URL to Access
+      credentials: true, // Allow Client To Send Cookies or Credentials to Server
+      allowedHeaders: ['Authorization', 'Content-Type', 'filename'], // Allow Only These Headers from Client to Server
+      exposedHeaders: ['filename', 'Content-Disposition'], // Expose Custom Headers to Client from Server
+      maxAge: 86400, // 1 Day
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow Only These Methods
+      optionsSuccessStatus: StatusCodes.CONTINUE, // Set Status Code for OPTIONS Request
+      preflightContinue: true, // Continue if OPTIONS Request
+    }),
+); // Compile Code Router
 
 // Link All Sub Routers to Main Router
-MainRouter.use('/process', CompileRouter) // Compile Code Router
+MainRouter.use('/process', CompileRouter); // Compile Code Router
 
 // Response Not Allowed Request
 MainRouter.all('*', (Request, Response) => {
@@ -38,10 +38,10 @@ MainRouter.all('*', (Request, Response) => {
       requestedUrl: Request.url,
       requestedMethod: Request.method,
       requestedBody: Request.body,
-      requestedHeaders: Request.headers
-    }
-  }) // Response Not Allowed Request
-}) // Not Allowed Request
+      requestedHeaders: Request.headers,
+    },
+  }); // Response Not Allowed Request
+}); // Not Allowed Request
 
 // Export Main Router
-export default MainRouter // Export Main Router as default
+export default MainRouter; // Export Main Router as default
