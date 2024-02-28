68599863e6c8ee9224521cff1a58ba4e6541a24f
Ankan Saha <ankansahadaluabari@gmail.com>
2024-02-28 06:21:30 +0000

    Update package.json and dependencies

--- server/source/core/index.core.js
+++ server/source/core/index.core.js
@@ -1,87 +1,33 @@
 // Server Related Imports
-import express, { json, urlencoded } from 'express' // Import Express
-import { Console, Middleware } from 'outers' // Import Console from outers
-import cluster from 'cluster' // Import Cluster
-import { StringKeys, NumberKeys, DatabaseKeys } from './environment variables.core.js' // Env Variables
-import { connect } from 'mongoose' // MongoSuper for Create Connection only
-
-// Middleware Imports
-import rateLimiter from '../Middleware/RateLimiter.middleware.js' // Express Rate Limiter
+import express, {json, urlencoded} from 'express' // Import Express
+import {methods} from 'outers' // Import Console from outers
+import {StringKeys, NumberKeys} from './environment variables.core.js' // Env Variables
 
 // Import Utils
 import Creator from '../utils/directoryCreator.utils.js' // Import Directory Creator
+import {ConnectDB} from '../Database/MongoDB.db.js'; // Import MongoDB Connection
 
 // Import Main Router
 import MainRouter from '../Routers/Router.js' // Import Main Router
 
 // Setup Express Server
 
-// CPU Count Copy for Process
-let ProcessCopy = NumberKeys.CPUCount // Copy CPU Count
+// Create Express Server Instance
+const Server = express() // Create Express Server Instance
 
-if (cluster.isPrimary) {
-  // Print CPU Count
-  Console.bright(
-    `${NumberKeys.CPUCount} CPU(s) detected With
-       ${StringKeys.Platform} ${StringKeys.Architecture} server : ${StringKeys.FreeRam} GB Free Ram : ${StringKeys.Model}`
-  )
+// Enable All Proxy Settings for Server Security
+Server.set('trust proxy', () => true) // Enable All Proxy Settings
 
-  // Fork Cluster
-  while (ProcessCopy > 0) {
-    cluster.fork()
-    ProcessCopy--
-  }
+// Enable JSON & URL Encoding Parser
+Server.use(json({limit: '999mb'})); // JSON Parser
+Server.use(urlencoded({extended: true, limit: '999mb', parameterLimit: 5000, inflate: true})); // URL Encoded Parser
 
-  // Listen for Cluster Online
-  cluster.on('online', (worker) => {
-    Console.green(`🚀 Worker ${worker.process.pid} started 🚀`)
-    Console.blue(`Environment Variables Loaded Successfully in Worker : ${worker.process.pid}`)
-    Console.yellow(`Worker ${worker.process.pid} is listening on Port ${NumberKeys.PORT}`)
-  })
+// Link All Router as MainRouter with all main middlewares
+Server.use('/api', MainRouter) // Link Main Router
 
-  // Listen for Cluster Exit
-  cluster.on('exit', (worker) => {
-    Console.red(`❌ Worker ${worker.process.pid} died ❌`)
-    cluster.fork()
-    Console.green(`🚀 Worker ${worker.process.pid} restarted 🚀`)
-    Console.blue(`Environment Variables Loaded Successfully in Worker : ${worker.process.pid}`)
-    Console.yellow(`Worker ${worker.process.pid} is listening on Port ${NumberKeys.PORT}`)
-  })
-} else {
-  // Create Express Server Instance
-  const Server = express() // Create Express Server Instance
+// Configure Static Folders
+Server.use(express.static(StringKeys.StaticDirectoryName)) // Configure Static Folder
+Server.use(express.static(StringKeys.InterpretedLangDirectoryName)) // Configure Static Folder
 
-  // Enable All Proxy Settings for Server Security
-  Server.set('trust proxy', () => true) // Enable All Proxy Settings
-
-  // Create a new URL object of Allowed URL
-  const AllowedURLobject = new URL(StringKeys.CORS_URL) // Create a new URL object of Allowed URL
-
-  // Link All Router as MainRouter with all main middlewares
-  Server.use(
-    '/api',
-    json({ limit: '999mb' }),
-    Middleware.AccessController([AllowedURLobject.hostname]),
-    urlencoded({ extended: true, limit: 5000000 * 1000, parameterLimit: 5000, inflate: true }),
-    rateLimiter, // Rate Limiter Middleware Function
-    Middleware.RequestInjectIP(['POST', 'PUT', 'DELETE']), // Inject IP Middleware Function
-    MainRouter // Main Router
-  ) // Link Main Router
-
-  // Configure Static Folders
-  Server.use(express.static(StringKeys.StaticDirectoryName)) // Configure Static Folder
-  Server.use(express.static(StringKeys.InterpretedLangDirectoryName)) // Configure Static Folder
-
-  // Create Directory
-  Creator() // Create Directory for Uncompiled Code
-
-  // Server Listen
-  try {
-    Server.listen(NumberKeys.PORT, async () => {
-      await connect(`${DatabaseKeys.MongoDB}${DatabaseKeys.DB_Name}`) // Connect to MongoDB
-      Console.green(`🚀 Database Connected & Server is listening on Port ${NumberKeys.PORT} 🚀`) // Print Message for Server Start
-    }) // Start Server on Port
-  } catch (error) {
-    Console.red(`Error in Starting Server : ${error}`) // Print Error Message for Server Start
-  }
-}
+// Start Server With Cluster Configuration
+methods.ClusterCreator(Server, NumberKeys.PORT, NumberKeys.CPUCount, [Creator], [ConnectDB]) // Create NodeJS Cluster Process
