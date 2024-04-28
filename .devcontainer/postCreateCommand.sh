4ffe16514a11000f9c0872d6f6fae1dbcdb1f91a
Ankan Saha <ankansahadaluabari@gmail.com>
2024-04-28 14:32:28 +0530

    Update .prettierrc, .gitignore, .eslintrc.yml, postCreateCommand.sh, commandExecutor.utils.js, CompileCode.Routes.js, Package-Installer.function.controllers.js, CORS.middleware.js, and directoryCreator.utils.js files

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y neofetch # Install curl
 
 # Install Dependencies
 cd client && npm install --force && npm run build && cd .. # Install client dependencies
-cd server && npm install --force && cd .. # Install server dependencies
+cd Code-Compiler_server && npm install --force && cd .. # Install server dependencies
 
 # Setup Docker Containers
 docker-compose up -d # Start Docker Containers in detached mode mention  in docker-compose.yml
